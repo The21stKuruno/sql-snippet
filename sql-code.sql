785de1e627db9c2d0a2c0effe3999b015c093bec
iamaian <The21stKuruno@yahoo.com>
2018-08-06 07:04:12 +0900

    create table

---
+++ sql-code.sql
@@ -0,0 +1,9 @@
+-- Creating a table
+
+CREATE TABLE tblimg (
+    idImg int(11) AUTO_INCREMENT PRIMARY KEY not null,
+    titleImg LONGTEXT not null,
+    descriptionImg LONGTEXT not null,
+    imgFullNameImg LONGTEXT not null,
+    orderImg LONGTEXT not null
+);
